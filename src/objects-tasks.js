e21cb4d4f34c324291233d42c0a4d22cc8945571
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-12-15 18:16:29 +0500

    feat: add sellTickets solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -151,8 +151,22 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const ticketPrice = 25;
+  let balance = 0;
+  return queue.every((payment) => {
+    if (payment > ticketPrice) {
+      const change = payment - ticketPrice;
+      if (balance >= change) {
+        balance -= change;
+        balance += ticketPrice;
+        return true;
+      }
+      return false;
+    }
+    balance += ticketPrice;
+    return true;
+  });
 }
 
 /**
